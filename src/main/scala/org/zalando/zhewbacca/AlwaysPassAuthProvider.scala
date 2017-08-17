7572de88fc4b5f08561eae1657fa2383399b033e
Dmitry Krivaltsevich <dmitry.krivaltsevich@zalando.de>
2017-08-17 12:03:45 +0200

    #23 explained main goal of the library in details

--- src/main/scala/org/zalando/zhewbacca/AlwaysPassAuthProvider.scala
+++ src/main/scala/org/zalando/zhewbacca/AlwaysPassAuthProvider.scala
@@ -2,6 +2,10 @@ package org.zalando.zhewbacca
 
 import scala.concurrent.Future
 
+/**
+  * This implementation is useful for Development environment to bypass a security mechanism.
+  * It assumes that every token is valid.
+  */
 class AlwaysPassAuthProvider(tokenInfo: TokenInfo) extends AuthProvider {
   override def valid(token: Option[OAuth2Token], scope: Scope): Future[AuthResult] =
     Future.successful(AuthTokenValid(tokenInfo))
